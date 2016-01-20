8c2db34d8e809750c486036766b692c54cf4848a
sunjintw <jinsun@thoughtworks.com>
2016-01-20 21:07:10 +0800

    [JS] add the basic unit tests for Profile class.

---
+++ test/iloveyouboss/ProfileTest.java
@@ -0,0 +1,40 @@
+package iloveyouboss;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by jsun on 1/14/16.
+ */
+public class ProfileTest {
+
+    @Test
+    public void return_false_when_matches_with_empty_criteria() {
+        Profile profile = new Profile("Test");
+        Criteria criteria = new Criteria();
+
+        boolean result = profile.matches(criteria);
+
+        assertThat(result, not(true));
+    }
+
+    @Test
+    public void return_true_when_criterion_weight_is_dont_care() {
+        Profile profile = new Profile("Test");
+        Answer answer = new Answer(new Question(0, null, null) {
+            @Override
+            public boolean match(int expected, int actual) {
+                return false;
+            }
+        }, 0);
+        Criterion criterion = new Criterion(answer, Weight.DontCare);
+        Criteria criteria = new Criteria();
+        criteria.add(criterion);
+
+        boolean result = profile.matches(criteria);
+        assertThat(result, is(true));
+    }
+
+}
